854d78a0166ee312d2b975d09b3bc63517353693
DYNBY <115410797+DYNBY@users.noreply.github.com>
2023-05-11 22:06:04 +0300

    fix: fixed part 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,46 +1,56 @@
-
 const fs = require('fs');
 const path = require('path');
-const sourceDir = path.join(__dirname, 'files');
-const destDir = path.join(__dirname, 'files-copy');
+const sourceDirPath = path.join(__dirname, 'files');
+const destDirPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(destDir, (err) => {
-  if (err && err.code !== 'EEXIST') {
-    throw err;
-  }
-
-  fs.readdir(sourceDir, (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-      const sourceFile = path.join(sourceDir, file);
-      const destFile = path.join(destDir, file);
-      fs.copyFile(sourceFile, destFile, (err) => {
-        if (err) throw err;
+async function copyDirectory() {
+  try {
+    await fs.promises.mkdir(destDirPath, { recursive: true });
+    fs.readdir(destDirPath, (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      files.forEach((file) => {
+        const filePath = path.join(destDirPath, file);
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error(err);
+          }
+        });
       });
     });
-  });
+    const entries = await fs.promises.readdir(sourceDirPath, { withFileTypes: true });
+    for (const entry of entries) {
+      const sourcePath = path.join(sourceDirPath, entry.name);
+      const destPath = path.join(destDirPath, entry.name);
 
-  fs.watch(sourceDir, { persistent: false }, (eventType, fileName) => {
-    const sourceFile = path.join(sourceDir, fileName);
-    const destFile = path.join(destDir, fileName);
-    if (eventType === 'rename') {
-      fs.stat(sourceFile, (err, stats) => {
-        if (err) {
-          fs.unlink(destFile, (err) => {
-            if (err && err.code !== 'ENOENT') {
-              throw err;
-            }
-          });
-        } else {
-          fs.copyFile(sourceFile, destFile, (err) => {
-            if (err) throw err;
-          });
-        }
-      });
-    } else if (eventType === 'change') {
-      fs.copyFile(sourceFile, destFile, (err) => {
-        if (err) throw err;
-      });
+      if (entry.isDirectory()) {
+        await copyDirectoryRecursive(sourcePath, destPath);
+      } else {
+        await fs.promises.copyFile(sourcePath, destPath);
+      }
     }
-  });
-});
+  } catch (err) {
+  }
+}
+async function copyDirectoryRecursive(sourcePath, destPath) {
+  try {
+    await fs.promises.mkdir(destPath, { recursive: true });
+    const entries = await fs.promises.readdir(sourcePath, { withFileTypes: true });
+    for (const entry of entries) {
+      const sourceEntryPath = path.join(sourcePath, entry.name);
+      const destEntryPath = path.join(destPath, entry.name);
+
+      if (entry.isDirectory()) {
+        await copyDirectoryRecursive(sourceEntryPath, destEntryPath);
+      } else {
+        await fs.promises.copyFile(sourceEntryPath, destEntryPath);
+      }
+    }
+  } catch (err) {
+  }
+}
+(async () => {
+  await copyDirectory();
+})();
